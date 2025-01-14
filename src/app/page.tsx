20a94997584f005d05d4fd9a379d69e33b9656bd
MostakemHossain <mostakememon123@gmail.com>
2025-01-14 08:47:25 +0600

    Project initilization next js and shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const page = () => {
+  return (
+    <div>
+      <Button variant={"destructive"}>Hello</Button>
+      <p className="text-5xl">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet
+      </p>
+    </div>
+  );
+};
+
+export default page;
