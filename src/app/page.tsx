9c5110b075a2e6b03d2754e638ed1cbb8388f890
MostakemHossain <mostakememon123@gmail.com>
2025-01-14 11:11:03 +0600

    components modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,7 @@ import { Button } from "@/components/ui/button";
 const page = () => {
   return (
     <div>
-      <Button variant={"destructive"}>Hello</Button>
-      <p className="text-5xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet
-      </p>
+      <Button variant={"muted"}>Hello</Button>
     </div>
   );
 };
